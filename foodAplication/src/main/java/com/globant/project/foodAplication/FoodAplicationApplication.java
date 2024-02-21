51c7771efb51ab0bc76783b2980f8eb6ea642255
Program0017 <andresxfernandezx@gmail.com>
2024-02-21 14:49:39 -0500

    second_commit

---
+++ foodAplication/src/main/java/com/globant/project/foodAplication/FoodAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.globant.project.foodAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAplicationApplication.class, args);
+	}
+
+}
---
+++ foodAplication/src/test/java/com/globant/project/foodAplication/FoodAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.project.foodAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
