8d50f31628fe6913509b03a91635c31332d32ea7
MiguelACoronado <miguelangelcoronadog@gmail.com>
2024-03-07 21:12:08 -0500

    Product put and patch swagger implement

--- foodAplication/src/main/java/com/globant/project/foodAplication/webApi/product/ProductController.java
+++ foodAplication/src/main/java/com/globant/project/foodAplication/webApi/product/ProductController.java
@@ -1,11 +1,17 @@
 package com.globant.project.foodAplication.webApi.product;
 
 import com.globant.project.foodAplication.commons.constants.endPoints.product.IProductEndPoints;
+import com.globant.project.foodAplication.commons.dto.ClientDto;
 import com.globant.project.foodAplication.commons.dto.ProductDtoRequest;
 import com.globant.project.foodAplication.commons.dto.ProductDto;
 import com.globant.project.foodAplication.mapper.ProductMapper;
+import com.globant.project.foodAplication.model.client.ClientEntity;
 import com.globant.project.foodAplication.model.product.ProductEntity;
 import com.globant.project.foodAplication.service.product.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,12 +39,20 @@ public class ProductController {
         ProductDto productDto = productService.findByUUID(uuid);
         return new ResponseEntity<ProductDto>(productDto, HttpStatus.OK);
     }
+
+    //-------------------------------------------------------------------//
+
+    @Operation(summary = "Uptade products", description = "Update products with UUID")
+    @ApiResponse(responseCode = "201", description = "Product Updated")
     @PutMapping(IProductEndPoints.PRODUCT_UPDATE_URL)
-    public ResponseEntity<ProductDto> updateProduct(@PathVariable("uuid") UUID uuid , @RequestBody ProductDto productDto){
-        ProductDto productDto1 = productService.updateProduct(uuid, productDto);
+    public ResponseEntity<ProductDto> updateProduct(@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Product to be updated", required = true, content = @Content(schema = @Schema(implementation = ProductEntity.class)))  @RequestBody ProductDto productDto, ProductDto uuid){
+        ProductDto productDto1 = productService.updateProduct(uuid.getUuid(), productDto);
         return new ResponseEntity<>(productDto1, HttpStatus.NO_CONTENT);
     }
 
+    //------------------------------------------------------------------//
+    @Operation(summary = "Inactivate Product", description = "Inactivate product, change avaliable to false")
+    @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductEntity.class)))
     @PatchMapping(IProductEndPoints.PRODUCT_DESACTIVATE_URL)
     public ResponseEntity<ProductDto> deleteProduct(@PathVariable("uuid") UUID uuid ){
         ProductDto productDto = productService.desactivateProduct(uuid);
